2827a20c26f7f77166fa4ed68585e8c875f7543d
Scott McLeod <scott.mcleod@theice.com>
2011-05-04 17:06:41 -0400

    Meet Rails Peepcode Stuff

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectManager::Application.config.secret_token = '4b2df4d48f8e8a6bf5daf422c2183baa89031e25cfe55eba35e0fbaafb9ccc642b9e19e27c17a91ad0f7730459a152862e2a58c86b179f0c85ed84dc319d256e'
